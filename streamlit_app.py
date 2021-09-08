ed678dadca6feb2eb12d1733f5d6409405fac9d4
BhushanGunjal <79009485+BhushanGunjal@users.noreply.github.com>
2021-09-09 04:17:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,17 @@ image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 clahe = cv2.createCLAHE(clipLimit = 4)
 final_img = clahe.apply(image_bw) 
+#_, ordinary_img = cv2.threshold(image_bw, 155, 201, cv2.THRESH_BINARY)
+
+
+#res = np.vstack((final_img, ordinary_img))
+#cv2.imshow('image', image)
+#cv2.imshow('image', final_img)
+#cv2.imshow('image', ordinary_img)
 
 
 
-
-cv2.imshow('ImageWindow', final_img)
-cv2.waitKey()
+#cv2.imshow('ImageWindow', final_img)
+st.image(final_img
+#cv2.waitKey()
 st.pyplot()
