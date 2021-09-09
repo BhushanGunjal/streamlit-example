58a57f812162ec31fc95ae5512de854d823ccabb
BhushanGunjal <79009485+BhushanGunjal@users.noreply.github.com>
2021-09-09 06:18:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,42 @@ if file is not None:
     st.image(final_img)
         #cv2.waitKey()
         #st.pyplot()
+        
+        image = cv2.imread('tom.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    x = st.slider('Change Threshold value',min_value = 50,max_value = 255)  
 
+    ret,thresh1 = cv2.threshold(image,x,255,cv2.THRESH_BINARY)
+    thresh1 = thresh1.astype(np.float64)
+    st.image(thresh1, use_column_width=True,clamp = True)
+    
+    st.text("Bar Chart of the image")
+    histr = cv2.calcHist([image],[0],None,[256],[0,256])
+    st.bar_chart(histr)
+    
+    st.text("Press the button below to view Canny Edge Detection Technique")
+    if st.button('Canny Edge Detector'):
+        image = load_image("jerry.jpg")
+        edges = cv2.Canny(image,50,300)
+        cv2.imwrite('edges.jpg',edges)
+        st.image(edges,use_column_width=True,clamp=True)
+      
+    y = st.slider('Change Value to increase or decrease contours',min_value = 50,max_value = 255)     
+    
+    if st.button('Contours'):
+        im = load_image("jerry1.jpg")
+          
+        imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(imgray,y,255,0)
+        image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        
+        img = cv2.drawContours(im, contours, -1, (0,255,0), 3)
+ 
+        
+        st.image(thresh, use_column_width=True, clamp = True)
+        st.image(img, use_column_width=True, clamp = True)
+         
 
 
 
