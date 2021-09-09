a141bc09a221ee56c305009b585b2524df6f3bd4
BhushanGunjal <79009485+BhushanGunjal@users.noreply.github.com>
2021-09-09 17:09:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,8 +89,10 @@ def photo():
             st.image(final_img)
             
             #im = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            #notworking
             clahe = cv2.createCLAHE(clipLimit = 4) 
             final_img = clahe.apply(img) 
+            #assertionerror comes here
             final_img = cv2.resize(final_img, (400, 400))
             st.write("")
             st.write("")
@@ -116,18 +118,18 @@ def photo1():
             st.write("")
             img = load_image(uploadFile)
             img = cv2.resize(img, (400, 400))
-            
+            #durvesh work on this
             x = st.slider('Change Threshold value',min_value = 69,max_value = 169)
             image = cv2.imread('image.png')
             image = cv2.resize(img, (300, 300))
             ret,thresh1 = cv2.threshold(image,x,255,cv2.THRESH_BINARY)
             thresh1 = thresh1.astype(np.float64)
             st.image(thresh1, use_column_width=True,clamp = True)
-    
+            #dhanashreesoln
             st.text("Bar Chart of the image")
             histr = cv2.calcHist([image],[0],None,[256],[0,256])
             st.bar_chart(histr)
-      
+            #final_imag is the name
             st.image(final_img)
         else:
             st.write("Make sure you image is in JPG/PNG Format.")
