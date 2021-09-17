a1afb30b395caf3f69fd152ee5be5b7e2a8d415d
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2021-09-17 20:54:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ img = st.file_uploader(label="Load X-Ray Chest image", type=['jpeg', 'jpg', 'png
 if img is not None:
             st.write("Original X-ray Image:")
             st.write("")
-            img = load_image(uploadFile)
             final_img = cv2.resize(img, (400, 400))
             st.image(final_img)
             
             #im = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
             #notworking
             clahe = cv2.createCLAHE(clipLimit = 4) 
-            final_img = clahe.apply(img) 
+            final_img = clahe.apply(final_img) 
             #assertionerror comes here
             final_img = cv2.resize(final_img, (400, 400))
             st.write("")
