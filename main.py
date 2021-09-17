15851f136085e71c76749c746f57f1c6f2a88cda
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2021-09-17 20:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,23 @@ from PIL import Image
 img = st.file_uploader(label="Load X-Ray Chest image", type=['jpeg', 'jpg', 'png'], key="xray")
 
 if img is not None:
-    # Preprocessing Image
-        st.image(img)
+            st.write("Original X-ray Image:")
+            st.write("")
+            img = load_image(uploadFile)
+            final_img = cv2.resize(img, (400, 400))
+            st.image(final_img)
+            
+            #im = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            #notworking
+            clahe = cv2.createCLAHE(clipLimit = 4) 
+            final_img = clahe.apply(img) 
+            #assertionerror comes here
+            final_img = cv2.resize(final_img, (400, 400))
+            st.write("")
+            st.write("")
+            st.write("After applying CLAHE:")
+            st.write("")
+      
+            st.image(final_img)
+else:
+            st.write("Make sure you image is in JPG/PNG Format.")
