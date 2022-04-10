3dde5fc7295f35c0ce1236871dbd295ff32bd8c2
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2022-04-11 04:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,27 +22,33 @@ def welcome():
 
 
 def photo():
-
-    
-
-        def load_image(img):
+    uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png', 'jpeg'])
+    def load_image(img):
             im = Image.open(img)
             image_array = np.array(im)
             return image_array
 
-        uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png', 'jpeg'])
-
-        if uploadFile is not None:
+    if uploadFile is not None:
             st.write("Original X-ray Image:")
             st.write("")
             img = load_image(uploadFile)
             
-   
             st.image(img)
-        else:
+    else:
             st.write("Make sure you image is in JPG/PNG Format.")
 
+            
+   # image = cv2.imread(ImageName) #Reading Image from path(/Image_Name.jpg) eg. here - /content/pneumonia bacterial.jpg
+   # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+  #  image_tensor = test_transforms(image=image)["image"]
+   # input_tensor = image_tensor.unsqueeze(0) 
+   # input_tensor = input_tensor.to(device)
+
+  #  loaded_model.eval()
+  #  prediction = np.argmax(loaded_model(input_tensor).detach().cpu().numpy())
+  #  Predicted_Class = idx_to_class[prediction]
+  #  st.write(Predicted_Class)
     
 if __name__ == "__main__":
     main()
