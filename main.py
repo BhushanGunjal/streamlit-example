62cd3017bb881c59d46ca88166b6bb6b0371e9c2
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2022-04-11 04:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,45 +22,9 @@ def welcome():
 
 
 def photo():
-    image = cv2.imread(ImageName) #Reading Image from path(/Image_Name.jpg) eg. here - /content/pneumonia bacterial.jpg
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    image_tensor = test_transforms(image=image)["image"]
-    input_tensor = image_tensor.unsqueeze(0) 
-    input_tensor = input_tensor.to(device)
-
-    loaded_model.eval()
-    prediction = np.argmax(loaded_model(input_tensor).detach().cpu().numpy())
-    Predicted_Class = idx_to_class[prediction]
-    st.write(Predicted_Class)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
     
+
         def load_image(img):
             im = Image.open(img)
             image_array = np.array(im)
