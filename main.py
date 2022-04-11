d2fa0b24e142a3462000e5500906fee8cc8391ba
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2022-04-11 14:31:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,8 +122,8 @@ def photo():
             st.write("")
             img = load_image(uploadFile)
             
-            image1 = img.resize((500,500))
-            st.image(image1)
+           # image1 = img.resize((500,500))
+            st.image(img)
             
             cv2.imwrite('1.jpg',img)
             #final_img0 = cv2.resize(img8, (255, 255))
