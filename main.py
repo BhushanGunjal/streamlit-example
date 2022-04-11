483f2c72e01973096777706b2010d993459b04fd
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2022-04-11 13:07:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,104 @@
 
+
 import streamlit as st
+from PIL import Image
+import cv2 
 import numpy as np
-from PIL import Image 
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import numpy as np
 from tensorflow.keras.models import load_model
-import tensorflow as tf
+np.random.seed(0)
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def loaded_model():
+  fp = "something.h5"
+  model_loader = load_model(fp)
+  return model_loader
+
+
+test_transforms = A.Compose(
+    [
+        A.SmallestMaxSize(max_size=512),
+        A.CenterCrop(height=512, width=512),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ]
+)
+
+
+def main():
+
+    selected_box = st.sidebar.selectbox(
+    'Choose one of the following',
+    ('Welcome','Detection')
+    )
+    
+    if selected_box == 'Welcome':
+        welcome()
+    if selected_box == 'Detection':
+        photo()
  
-from tempfile import NamedTemporaryFile
-from tensorflow.keras.preprocessing import image 
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-@st.cache(allow_output_mutation=True)
+def welcome():
+    _, col2, _ = st.columns([1, 10, 1])
 
-temp = st.file_uploader("Upload X-Ray Image")
-#temp = temp.decode()
+    with col2:
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.title('Covid 19 Detection using X-ray')
+    with col2:
+        st.write("")
+        st.write("")
+        st.write("")
+        st.subheader('Dhanashree Chavan')
+        st.write("")
+        st.subheader('Bhushan Gunjal')
+        st.write("")
+        st.subheader('Durvesh Talekar')
+    
 
-buffer = temp
-temp_file = NamedTemporaryFile(delete=False)
-if buffer:
-    temp_file.write(buffer.getvalue())
-    st.write(image.load_img(temp_file.name))
 
+def photo():
+    uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png', 'jpeg'])
+    def load_image(img):
+            im = Image.open(img)
+            image_array = np.array(im)
+            return image_array
 
-if buffer is None:
-  st.text("Oops! that doesn't look like an image. Try again.")
+    if uploadFile is not None:
+            st.write("Original X-ray Image:")
+            st.write("")
+            img = load_image(uploadFile)
+            
+            st.image(img)
+            
+            cv2.imwrite('1.jpg',img)
+            #final_img0 = cv2.resize(img8, (255, 255))
+            #im = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            image = cv2.imread(r'1.jpg')
 
-else:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
- 
+            image_tensor = test_transforms(image=image)["image"]
+            input_tensor = image_tensor.unsqueeze(0) 
+          #  input_tensor = input_tensor.to(device)
 
-  hardik_img = image.load_img(temp_file.name, target_size=(500, 500),color_mode='grayscale')
+            loaded_model.eval()
+            prediction = np.argmax(load_model("something.h5")(input_tensor).detach().cpu().numpy())
+            Predicted_Class = idx_to_class[prediction]
+            st.write(Predicted_Class)
+    else:
+            st.write("Make sure you image is in JPG/PNG Format.")
 
-  # Preprocessing the image
-  pp_hardik_img = image.img_to_array(hardik_img)
-  pp_hardik_img = pp_hardik_img/255
-  pp_hardik_img = np.expand_dims(pp_hardik_img, axis=0)
+            
 
-  #predict
-  #hardik_preds= cnn.predict(pp_hardik_img)
-
-  #out = hardik_preds
-  
-
- # st.success(out)
-  
-  st.image(pp_hardik_img,use_column_width=True)
-          
-       
-
+    
+if __name__ == "__main__":
+    main()
 
